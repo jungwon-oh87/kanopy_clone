e88b9643e319acc09407d7db0f268c4c488b6921
Jungwon Oh <jungwon.oh87@gmail.com>
2020-06-07 21:19:01 -0700

    fetching two api but latest movies need to be fixed. added bordered button and slick.

--- src/App.js
+++ src/App.js
@@ -9,8 +9,42 @@ import colette_img from "./assets/images/lp/colette.jpg";
 import eco_img from "./assets/images/lp/eco.jpg";
 import florida_img from "./assets/images/lp/florida.jpg";
 import call_img from "./assets/images/lp/call.jpg";
+import BorderedButton from "./components/bordered-button/borderedButton.component";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      popularMovies: [],
+      latestMovies: [],
+    };
+  }
+
+  componentDidMount() {
+    const combinedData = { request1: {}, request2: {} };
+    Promise.all([
+      fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      ).then((value) => value.json()),
+      fetch(
+        `https://api.themoviedb.org/3/movie/latest?api_key=${API_KEY}&language=en-US`
+      ).then((value) => value.json()),
+    ]).then((data) => {
+      const popular = data[0];
+      const latest = data[1];
+      console.log("checking data[1]: ", latest);
+      this.setState(
+        { popularMovies: popular.results, latestMovies: latest.results },
+        () => {
+          console.log("checking state: ", this.state.popularMovies);
+          console.log("checking state: ", this.state.latestMovies);
+        }
+      );
+    });
+  }
+
   render() {
     const settings = {
       dots: false,
@@ -21,9 +55,21 @@ class App extends React.Component {
       draggable: false,
     };
 
+    const call_action_settings = {
+      dots: false,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 5000,
+      autoplaySpeed: 0,
+      cssEase: "linear",
+    };
+
     return (
-      <div className="grid grid-cols-1">
-        <section className="bg-black relative">
+      <div className="grid grid-cols-1 bg-black">
+        <section className="relative">
           {/* <button>Log in</button> */}
           <Slider {...settings} className="w-full z-0">
             <div className="w-full h-screen">
@@ -83,22 +129,65 @@ class App extends React.Component {
           
         </div> */}
         </section>
-        <section className="bg-black relative">
+        <section className="relative">
           {/* <button>Log in</button> */}
           <div className="w-full z-0">
             <img className="w-full" alt="film_img" src={call_img} />
           </div>
-          <div className="absolute w-1/3 right-0 top-0 flex flex-col mr-6 z-1">
-            <h1 className="text-white font-semibold text-2xl tracking-wider mt-20 mb-10 leading-tight">
+          <div className="absolute w-2/5 right-0 top-0 flex flex-col mr-20 z-1">
+            <h1 className="text-white font-semibold text-4xl tracking-wider leading-tight mt-20 mb-10">
               Start streaming movies today with your public library card or
               university login
             </h1>
-            <p className="text-white font-thin text-xl tracking-wider mb-10 ">
+            <p className="text-white font-thin text-lg tracking-wider mb-10 ">
               We partner with public libraries and universities to bring you
               ad-free films and series that can be enjoyed on your TV, mobile
               phone, tablet and online.
             </p>
-            <StartButton width="w-1/2">Get Started</StartButton>
+            <StartButton width="w-2/5">Get Started</StartButton>
+          </div>
+        </section>
+        <section>
+          <div className="text-center text-white font-semibold text-4xl tracking-wider leading-tight mt-20 mb-20">
+            Enjoy critically-acclaimed movies,
+            <br /> inspiring documentaries, award-winning <br /> foreign films
+            and more
+          </div>
+          <Slider {...call_action_settings}>
+            {this.state.popularMovies.map((m) => {
+              return (
+                <div className="w-full h-auto border-solid" key={m.id}>
+                  <img
+                    className="w-4/5 h-full m-auto object-cover"
+                    alt="film_img"
+                    src={`http://image.tmdb.org/t/p/w154/` + m.poster_path}
+                  />
+                </div>
+              );
+            })}
+          </Slider>
+          <br />
+          <Slider {...call_action_settings} speed={7000}>
+            {this.state.popularMovies.map((m) => {
+              return (
+                <div className="w-full h-auto border-solid" key={m.id}>
+                  <img
+                    className="w-4/5 h-full m-auto object-cover"
+                    alt="film_img"
+                    src={`http://image.tmdb.org/t/p/w154/` + m.poster_path}
+                  />
+                </div>
+              );
+            })}
+          </Slider>
+          <div className="w-1/2 m-auto mt-10 mb-10">
+            <p className="text-white text-center font-semibold text-3xl tracking-wider leading-tight">
+              Stream these movies and thousands more
+            </p>
+            <br />
+            <div className="w-1/3 m-auto">
+              <BorderedButton>Browse Movies</BorderedButton>
+            </div>
           </div>
         </section>
       </div>
